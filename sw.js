8e047e3fa9312328dd2b8f246ea62cd99717ee1a
davors-gh <davorsulava@gmail.com>
2026-09-10 21:59:52 +1000

    Fix anime search and artwork refresh

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='anime-tracker-shell-v2';
+const CACHE='anime-tracker-shell-v3';
 const ASSETS=['./','./index.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
