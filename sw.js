cd5ffad89c0a2fb63136a1e8e984cfece0c219f5
davors-gh <davorsulava@gmail.com>
2026-09-10 21:24:26 +1000

    Upgrade Anime Tracker and add offline support

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE='anime-tracker-shell-v1';
+const ASSETS=['./','./index.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
+});
