dd193a7907690ba6c05c54aba9c653d3f9db0980
davors-gh <davorsulava@gmail.com>
2026-09-10 21:47:53 +1000

    Add season and episode lookup

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='anime-tracker-shell-v1';
+const CACHE='anime-tracker-shell-v2';
 const ASSETS=['./','./index.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;
   e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));
