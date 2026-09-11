37b1be90ffd67f1ed7cc2c0173e023e5a349d54f
davors-gh <davorsulava@gmail.com>
2026-09-11 20:38:25 +1000

    Harden iPhone poster touch handling

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE='anime-tracker-shell-v16';
+const CACHE='anime-tracker-shell-v17';
 const ASSETS=['./','./index.html'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET'||new URL(e.request.url).origin!==location.origin)return;
