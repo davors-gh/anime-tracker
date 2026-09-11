6b1ba685f4bdcfb52d1dd8aa8abd1596ab792c7a
davors-gh <davorsulava@gmail.com>
2026-09-11 21:19:25 +1000

    Fix Continue Watching quick add

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='anime-tracker-shell-v20';
+const CACHE='anime-tracker-shell-v21';
 const ASSETS=['./','./index.html','./next-episode-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
