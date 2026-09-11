5fe25cfc7a30f1fcaab320613be8f1ace945e0c2
davors-gh <davorsulava@gmail.com>
2026-09-11 21:40:06 +1000

    Apply EP+ branding

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='anime-tracker-shell-v22';
-const ASSETS=['./','./index.html','./next-episode-icon.png'];
+const CACHE='anime-tracker-shell-v23';
+const ASSETS=['./','./index.html','./ep-plus-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
